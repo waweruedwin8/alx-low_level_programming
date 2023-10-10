6e063149d4c1491c98204029cff786247ec302ea
root <waweruedwin8@gmail.com>
2023-10-10 03:44:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,13 +5,7 @@
  * return - always 0
  */
 int main(void)
-<<<<<<< HEAD
 {char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
 puts(Mystr);
-return (0);/* returns always 0*/
-=======
-{char Mystr[] = " \"Programming is like building a multilingual puzzle\n";
-puts(Mystr);
-return (0);/* always return 0*/
->>>>>>> 52bff56ef66c4f9c8a35ba305e9e08cd71f18ee4
+return (0);/* always returns 0*/
 }
