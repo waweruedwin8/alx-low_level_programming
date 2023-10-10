690e7b564cc6cf3b8bb3d7e8d3f0adee856577c6
root <waweruedwin8@gmail.com>
2023-10-10 03:09:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function
  * return - always 0
  */
-int main (void)
-{ 
-    
-    char Mystr[] = " \"Programming is like building a multilingual puzzle";    
-    puts(Mystr);
-        return (0);
+int main(void)
+{char Mystr[] = " \"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
