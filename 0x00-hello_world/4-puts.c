9c99ae0c1f957ae97e3b55e1b79234114f02dd21
root <waweruedwin8@gmail.com>
2023-10-10 02:09:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * return - always 0
+ */
+int main (void)
+{ puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
