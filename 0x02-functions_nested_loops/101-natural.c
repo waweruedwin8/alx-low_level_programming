62727621714a42f961edba1565ff8e647963d33f
root <waweruedwin8@gmail.com>
2023-10-11 16:12:10 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,12 +10,15 @@
 
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	int i, z = 0;
+	while (i <1024)
+	{
+	if (( i % 3 == 0) || (i % 5 == 0 ))
+	{
+	z += i;
+	}
+	i++
+	}
+	printf("%d\n",z);
 	return (0);
 }
