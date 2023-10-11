386f839f339ccca32293aff70d44d0218136ea9a
root <waweruedwin8@gmail.com>
2023-10-11 13:19:20 -0700

    5-main.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign no
+ * @n: the no to be checked
+ * Return: 1 for +num, -1 for negative num or zero for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	return (0);
+	}
+}
