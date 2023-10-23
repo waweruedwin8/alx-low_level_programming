63a1d50432b40cfe7787b0e5d56b526ad26f5693
root <waweruedwin8@gmail.com>
2023-10-23 12:46:23 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - entry point
- * @s: input
- * @to: input
- * Return: void
- */
+* set_string - entry point
+* @s: input
+* @to: input
+* Return: void
+*/
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
 
