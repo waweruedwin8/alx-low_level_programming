3e348db1e7568cd611b43656849715c8cf7ba3e9
root <waweruedwin8@gmail.com>
2023-10-31 14:25:59 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,30 +3,30 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
- */
+* _strdup - duplicate to new memory space location
+* @str: char
+* Return: 0
+*/
 char *_strdup(char *str)
 {
-    char *aaa;
-    int i, r = 0;
+char *aaa;
+int i, r = 0;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
+i = 0;
+while (str[i] != '\0')
+i++;
 
-    aaa = malloc(sizeof(char) * (i + 1));
+aaa = malloc(sizeof(char) * (i + 1));
 
-    if (aaa == NULL)
-        return (NULL);
+if (aaa == NULL)
+return (NULL);
 
-    for (r = 0; str[r]; r++)
-        aaa[r] = str[r];
+for (r = 0; str[r]; r++)
+aaa[r] = str[r];
 
-    return (aaa);
+return (aaa);
 }
 
