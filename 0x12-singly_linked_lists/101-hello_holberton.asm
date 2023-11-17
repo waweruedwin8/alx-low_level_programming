e93e159ef5467d242eaa6804ca41920d4c5240bd
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-16 16:39:11 -0800

    innit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-section .data
-format db 'Hello, Holberton', 0
-
-section .text
-global main
-extern printf
-
+       global    main
+          extern    printf
 main:
-mov edi, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
