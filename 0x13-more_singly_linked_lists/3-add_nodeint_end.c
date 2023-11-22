ff852d923e2228a3d9b17da9c71528f53fcad82c
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-21 17:09:03 -0800

    innit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @data: integer data to insert in the new element
- *
- * Return: pointer to the new node, or NULL if it fails
- */
+* add_nodeint_end - adds a node at the end of a linked list
+* @head: pointer to the first element in the list
+* @n: int
+* Return: pointer to the new node, or NULL if it fails
+*/
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
