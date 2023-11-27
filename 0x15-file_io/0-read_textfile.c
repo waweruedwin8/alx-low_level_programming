ad51aacce957270949970282ee00677b1706d9b6
waweruedwin8 <waweruedwin8@gmail.com>
2023-11-27 14:01:46 -0800

    innit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ read_textfile - Read text file print to STDOUT.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: w - actual number of bytes read and printed
+*         0 when function fails or filename is NULL.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer;
+ssize_t file_descriptor;
+ssize_t bytes_read;
+ssize_t bytes_written;
+
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+bytes_read = read(file_descriptor, buffer, letters);
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+free(buffer);
+close(file_descriptor);
+return (bytes_written);
+}
